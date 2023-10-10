d3f07c24fe90e14eb8accc6a8d83edac56ad320d
G00400498@atu.ie <g00400498@atu.ie>
2023-10-10 15:51:28 +0100

    Added code to UserController Class. Created both UserService and EmailService classes.

--- src/main/java/ie/atu/usingiocanddi/UserController.java
+++ src/main/java/ie/atu/usingiocanddi/UserController.java
@@ -1,4 +1,17 @@
 package ie.atu.usingiocanddi;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+
+    private UserService us;
+    public UserController() { us = new UserService();}
+    @GetMapping("/registerUser/{name}/{email}")
+    public String getUser(@PathVariable String name, @PathVariable String email) {
+        us.registerUser(name, email);
+        return "Without IoC";
+    }
 }

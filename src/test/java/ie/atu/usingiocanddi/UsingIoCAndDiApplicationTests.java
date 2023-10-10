3f855f584cbe187f3fd433eb17f286010b0d8217
G00400498@atu.ie <g00400498@atu.ie>
2023-10-10 15:36:59 +0100

    Created UserController Class

---
+++ src/test/java/ie/atu/usingiocanddi/UsingIoCAndDiApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.usingiocanddi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsingIoCAndDiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

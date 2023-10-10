6af4abc0fd9f8e88b24b2122ed29f8234003481f
G00400498@atu.ie <g00400498@atu.ie>
2023-10-10 16:07:15 +0100

    Added code to UserService and EmailService classes.

--- src/main/java/ie/atu/usingiocanddi/EmailService.java
+++ src/main/java/ie/atu/usingiocanddi/EmailService.java
@@ -1,4 +1,8 @@
 package ie.atu.usingiocanddi;
 
 public class EmailService {
+    public void sendEmail(String to, String message) {
+        //code to send email
+        System.out.println("Your name is " + to + "Your email is " + message);
+    }
 }
